d367dce764d406fc4562b8be3b0f5b123f008577
Oli <dreiwutz@googlemail.com>
2023-02-25 10:28:22 +0100

    insta feed

--- src/components/Verein.js
+++ src/components/Verein.js
@@ -1,3 +1,6 @@
+import { ElfsightWidget } from 'react-elfsight-widget';
+
 export default function Verein() {
-    return <h1>Verein</h1>;
+    return <ElfsightWidget widgetID="e7b431ca-7d0d-4ef1-9ab8-ba42cb081145" />
+    ;
 }
