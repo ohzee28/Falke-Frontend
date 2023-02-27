8a9f704409ac9d59da14494de1bd32f6af72b767
Oli <dreiwutz@googlemail.com>
2023-02-27 10:46:34 +0100

    montag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import logo from "../images/Logo Falke 1.png";
 
 export default function Navbar() {
     return <nav className="nav">
+        <img href="/" className="falkelogo" src={logo} alt="Logo" />
         <a href="/" className="fsvfalke95">FSVFALKE95</a>
         <ul>
             <li className="active"><Link to="/news">NEWS</Link></li>
