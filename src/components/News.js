1b70e1afa86a3475b0036b1bdf5508f2d7e1cca8
Oli <dreiwutz@googlemail.com>
2023-02-27 17:52:44 +0100

    Artikel fetch

--- src/components/News.js
+++ src/components/News.js
@@ -1,3 +1,30 @@
-export default function News() {
-    return <h1>News</h1>;
+import { useEffect, useState } from "react";
+
+const News = () => {
+    const [articles, setArticles] = useState(null)
+
+    useEffect(() => {
+        const fetchArticles = async () => {
+            const response = await fetch('https://falkebackend.onrender.com/articles')
+            const json = await response.json()
+
+            if (response.ok) {
+                setArticles(json)
+            }
+
+        }
+        fetchArticles()
+    }, [])
+
+    return (
+        <div className="news">
+            <div className="articles">
+            {articles && articles.map((article) => (
+                <p key={article._id}>{article.headline}</p>
+            ))}
+            </div>
+        </div>
+    )
 }
+
+export default News
