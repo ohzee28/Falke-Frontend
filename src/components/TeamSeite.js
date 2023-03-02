f86b25fac385881ecde62e84161c62aee5da1483
Oli <dreiwutz@googlemail.com>
2023-03-02 16:40:02 +0100

    widgets

--- src/components/TeamSeite.js
+++ src/components/TeamSeite.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useParams, Link } from 'react-router-dom';
 
 function TeamSeite({ teams }) {
@@ -10,15 +10,43 @@ function TeamSeite({ teams }) {
     // console.log(useParams())
     console.log(thisTeam);
 
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src =
+            "https://www.fussball.de/static/layout/fbde2/egm//js/widget2.js";
+        script.async = true;
+        document.head.appendChild(script);
+
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if (typeof fussballdeWidgetAPI !== "undefined") {
+                clearInterval(intervalId);
+                new window.fussballdeWidgetAPI().showWidget(
+                    "widget1",
+                    `${thisTeam.websitekeys[0]}`
+                );
+            }
+        }, 100);
+    }, []);
+
+    
+
     return thisTeam ? (
         <div className="teamcontainer">
+         <div id="widget1"></div>
             <div className="teamname">
                 <h2>{thisTeam.teamname}</h2>
             </div>
             <div className="teamfoto">
                 <img src={thisTeam.image} alt="Foto" />
             </div>
-            
+
 
             <Link to="/teams">
                 <button className="button">Zurück zur Teamübersicht</button>
