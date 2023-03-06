50a7e14795c4b68d06503bc89afe98ac67449b80
Oli <dreiwutz@googlemail.com>
2023-03-06 21:07:12 +0100

    Historie & Impressum

--- src/components/Historie.js
+++ src/components/Historie.js
@@ -1,3 +1,20 @@
-export default function Historie() {
-    return <h1>Historie</h1>;
+import "./HistorieStyles.css"
+
+function Historie (){
+    return(
+        <div className="historie-container">
+            <h1>Geschichte</h1>
+            <h2>Gründung 1995</h2>
+            <p>Nach der Gründungsversammlung im Juni und der darauffolgenden Eintragung in das Vereinsregister am 21.11.1995 entstand durch den Zusammenschluss der Vorgängerverereine "SG 1962 Falkenrehde" und der "BSG Lokomotive Ketzin" der FSV 1995 Ketzin/Falkenrehde e.V. 
+            </p>
+            <h2>Früher Erfolg</h2>
+            <p>Der bislang größte Erfolg ist der Aufstieg der Ersten Männermannschaft aus der Landesliga in die Brandenburgliga im Jahr 1996. Vier Jahre konnte man sich in der höchsten Spielklasse in Brandenburg halten. In der Saison 1999/2000 erfolgte dann der Abstieg in die Landesliga.</p>
+            <h2>Seit 2016 in der Kreisliga</h2>
+            <p>Nachdem man lange Zeit in den 00er Jahren in der Landesklasse verbracht hat ging es danach bis in die Kreisklasse. Durch 2 aufeinanderfolgene Aufstiege spielt die Erste Männermannschaft seit der Saison 2016/2017 durchgehend in der Kreisliga A Havelland. In der Saison 2021/2022 schaffte man es lange an der Tabellenspitze zu stehen und landete am Ende auf dem 3. Tabellenplatz.</p>
+        </div>
+    )
+
 }
+
+
+export default Historie
--- src/components/Impressum.js
+++ src/components/Impressum.js
@@ -1,3 +1,28 @@
-export default function Impressum() {
-    return <h1>Impressum</h1>;
+import "./ImpressumStyles.css"
+
+function Impressum() {
+    return (
+        <div className="impressum-container">
+            <h1>Impressum</h1>
+            <h2>FSV ’95 Ketzin/Falkenrehde e.V.</h2>
+            <h3>Der Vorstand</h3>
+            <p>Zwei Vorstandsmitglieder sind gemeinschaftlich vertretungsbefugt:
+                Uwe Liebig, Frank Recknagel (1.Vors.), Sebastian Schläger (2. Vors.), Donald Schwarz, Corinna Stackebrandt
+            </p>
+            <h3>Postanschrift: Paretzer Weg 17, 14669 Ketzin/Havel</h3>
+            <h3>Telefon: 033233/18464</h3>
+            <h3>eingetragen im Vereinsregister beim Amtsgericht Potsdam VR 5350</h3>
+            <h3>Sitz des Vereins: Friedrich-Ludwig-Jahn-Weg 32, 14669 Ketzin/Havel</h3>
+            <h3>Verantwortlicher i.S.d. § 55 Abs. 2 RStV Frank Recknagel, Paretzer Weg 17, 14669 Ketzin/Havel</h3>
+            <h3>Anbieterkennzeichnung nach § 5 TMG</h3>
+            <h3>Dieses Impressum gilt auch für die offiziellen Seiten in sozialen Medien</h3>
+            <h3>Soweit auf unserer Seite links zu anderen Seiten führen, haften wir nicht für deren Rechtswidrigkeit. Wir prüfen die links sorgfältig, bevor wir sie setzen, aber für deren Veränderung nach dem Veröffentlichungsdatum können wir nicht garantieren.</h3>
+            <h3>Verantwortlich für die Erstellung dieser Website: Oliver Zöllner</h3>
+            <p></p>
+        </div>
+    )
+
 }
+
+
+export default Impressum
